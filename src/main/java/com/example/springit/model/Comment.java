aafa4881d637c351ce4fb54d4fd366db52b36ccf
John Werenski <jtwerenski@gmail.com>
2020-01-13 14:00:56 -0500

    Create Comment and Vote model classes, and remove code from Link.

---
+++ src/main/java/com/example/springit/model/Comment.java
@@ -0,0 +1,15 @@
+package com.example.springit.model;
+
+public class Comment {
+	
+	private Long id;
+	private String body;
+	
+	public Comment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//link	
+
+}
---
+++ src/main/java/com/example/springit/model/Vote.java
@@ -0,0 +1,16 @@
+package com.example.springit.model;
+
+public class Vote {
+	
+	private Long id;
+	private int vote;
+	
+	//user and link
+	
+	public Vote() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
