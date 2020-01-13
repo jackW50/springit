e85299a26d735148ad140145b20daf4af2aea1de
John Werenski <jtwerenski@gmail.com>
2020-01-13 14:11:27 -0500

    Add annotations to models.

--- src/main/java/com/example/springit/model/Link.java
+++ src/main/java/com/example/springit/model/Link.java
@@ -1,7 +1,14 @@
 package com.example.springit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Link {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String title;
 	private String url;
@@ -41,6 +48,50 @@ public class Link {
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
+	@Override
+	public String toString() {
+		return "Link [id=" + id + ", title=" + title + ", url=" + url + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Link other = (Link) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+	
+	
 
 	
 }
