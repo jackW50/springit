02dc0cc90d5d70aba81662490e183c786a689e08
John Werenski <jtwerenski@gmail.com>
2020-01-16 12:01:10 -0500

    Create main and new page layouts and create a foo example.

--- src/main/java/com/example/springit/controller/LinkController.java
+++ src/main/java/com/example/springit/controller/LinkController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,42 +18,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.springit.domain.Link;
 import com.example.springit.repository.LinkRepository;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
 	
-	private LinkRepository linkRepository;
-	
-	@Autowired
-	public LinkController(LinkRepository linkRepository) {
-		this.linkRepository = linkRepository;
+//	private LinkRepository linkRepository;
+//	
+//	@Autowired
+//	public LinkController(LinkRepository linkRepository) {
+//		this.linkRepository = linkRepository;
+//	}
+//	
+	@GetMapping("/foo")
+	public String foo(Model model) {
+		model.addAttribute("pageTitle", "This page is FOO!");
+		return "foo";
 	}
-	
-	//list
-	@GetMapping("/")
-	public List<Link> list() {
-		return linkRepository.findAll();
-	}
-	
-	//CRUD
-	@PostMapping("/create")
-	public Link create(@ModelAttribute Link link) {
-		return linkRepository.save(link);
-	}
-	
-	@GetMapping("/{id}")
-	public Optional<Link> read(@PathVariable Long id) {
-		return linkRepository.findById(id);
-	}
-	
-	@PutMapping("/{id}")
-	public Link update(@ModelAttribute Link link) {
-		return linkRepository.save(link);
-	}
-	
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id) {
-		linkRepository.deleteById(id);
-	}
+//	
+//	//list
+//	@GetMapping("/")
+//	public List<Link> list() {
+//		return linkRepository.findAll();
+//	}
+//	
+//	//CRUD
+//	@PostMapping("/create")
+//	public Link create(@ModelAttribute Link link) {
+//		return linkRepository.save(link);
+//	}
+//	
+//	@GetMapping("/{id}")
+//	public Optional<Link> read(@PathVariable Long id) {
+//		return linkRepository.findById(id);
+//	}
+//	
+//	@PutMapping("/{id}")
+//	public Link update(@ModelAttribute Link link) {
+//		return linkRepository.save(link);
+//	}
+//	
+//	@DeleteMapping("/{id}")
+//	public void delete(@PathVariable Long id) {
+//		linkRepository.deleteById(id);
+//	}
 
 }
