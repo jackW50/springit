3d3b35c70d048bbca166a047093ca7bb9c62e185
John Werenski <jtwerenski@gmail.com>
2020-01-16 11:01:59 -0500

    Create a GetMapping int he home controller to the home route to return the home template.

--- src/main/java/com/example/springit/controller/HomeController.java
+++ src/main/java/com/example/springit/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.example.springit.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 /*
  * @Controller and @ResponseBody
  */
-@RestController 
+@Controller 
 public class HomeController {
 
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title", "Hello, Thymeleaf!"); 
+		return "home";
+	}
 }
