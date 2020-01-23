5d0b7bcda4ea4c75cb3c1dd43df101f28108590d
John Werenski <jtwerenski@gmail.com>
2020-01-23 10:47:32 -0500

    Override the configure method in the security configuration class, and create a UserDetailsServiceImpl that can find a user by email.

--- src/main/java/com/example/springit/repository/UserRepository.java
+++ src/main/java/com/example/springit/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.springit.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springit.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	
+	Optional<User> findByEmail(String email);
 }
--- src/main/java/com/example/springit/security/SecurityConfiguration.java
+++ src/main/java/com/example/springit/security/SecurityConfiguration.java
@@ -1,14 +1,23 @@
 package com.example.springit.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.example.springit.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	private UserDetailsServiceImpl userDetailsService;
+	
+	public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
@@ -21,6 +30,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin();
 			
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
 	}
 	
 	
